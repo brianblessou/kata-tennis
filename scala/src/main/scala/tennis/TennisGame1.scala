288739427dcbc0bd54bebd1e86dfbab0cc2a2956
robin <robin.cavalieri@businessdecision.be>
2019-10-11 11:12:16 +0200

    Add Java documentation post refact.

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -14,7 +14,7 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
 
   /**
    *
-   * @param score
+   * @param score Int between 0 and 2
    * @return scoreConv
    * @auth Robin CAVALIERI
    * @desc
@@ -31,8 +31,8 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
 
   /**
    *
-   * @param scorePlayer1
-   * @param scorePlayer2
+   * @param scorePlayer1 Int between 0 and 2
+   * @param scorePlayer2 Int between 0 and 2
    * @return score
    * @author Robin CAVALIERI
    * @desc
@@ -41,25 +41,25 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
     var score = ""
     val minusResult = scorePlayer1-scorePlayer2
 
-    if (minusResult==1) {
+    if (minusResult == 1) {
       score = "Advantage player1"
     }
     else if (minusResult == -1) {
-      score ="Advantage player2"
+      score = "Advantage player2"
     }
-    else if (minusResult>=2) {
+    else if (minusResult >= 2) {
       score = "Win for player1"
     }
     else {
-      score ="Win for player2"
+      score = "Win for player2"
     }
     score
   }
 
   /**
    *
-   * @param scorePlayer1
-   * @param scorePlayer2
+   * @param scorePlayer1 Int between 0 and 2
+   * @param scorePlayer2 Int between 0 and 2
    * @return
    * @author Robin CAVALIERI
    * @desc
@@ -73,7 +73,7 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
         tempScore = scorePlayer1
       }
       else {
-        score+="-";
+        score += "-"
         tempScore = scorePlayer2
       }
       val tempScore2 = tempScore match {
@@ -89,12 +89,13 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
 
   /**
    *
+   * @desc depending on the case call diffScore / winningScore / equalScore
    * @return
    */
   def calculateScore() : String = {
       var score : String = ""
 
-      if (m_score1==m_score2)
+      if (m_score1 == m_score2)
       {
           score = equalScore(m_score1)
       }
