17027cddb1e7697f30815a9e343ad4e5a313f91e
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 16:51:33 +0200

    Tennis3: remove game from Deuce Score object as is not needed

--- python/src/TennisGame3/return_deuce.py
+++ python/src/TennisGame3/return_deuce.py
@@ -2,17 +2,14 @@ from python.src.TennisGame3 import Scorer
 
 
 class ReturnDeuce(Scorer):
-    def __init__(self, game: object):
+    def __init__(self):
         """
-        Scorer class that computes score when points are lower than 4
-        :param game: object instance of TennisGame3 of which to compute the score (object)
+        Scorer class that returns 'Deuce' score
         """
-        self.pointsPlayer1 = game.pointsPlayer1
-        self.pointsPlayer2 = game.pointsPlayer2
 
     def compute_score(self) -> str:
         """
-        Compute score when both players have scores lower than 4, except when 3-3
+        Compute score when both players have same scores bigger than 2
         :return: Actual score (str)
         """
         return 'Deuce'
--- python/src/TennisGame3/score_creator.py
+++ python/src/TennisGame3/score_creator.py
@@ -14,9 +14,13 @@ class ScoreCreator(Factory):
         self.pointsPlayer1 = game.pointsPlayer1
         self.pointsPlayer2 = game.pointsPlayer2
 
-    def create(self):
+    def create(self) -> object:
+        """
+        Create a Score object depending on the points logic
+        :return: Score object
+        """
         if (self.pointsPlayer1 == self.pointsPlayer2) and (self.pointsPlayer1 >= 3):
-            return ReturnDeuce(self.game)
+            return ReturnDeuce()
         elif self.pointsPlayer1 < 4 and self.pointsPlayer2 < 4:
             return PointsLowerThan4(self.game)
         else:
