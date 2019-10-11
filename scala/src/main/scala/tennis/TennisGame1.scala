26290080e2f6d277ec7c867be0cf86264481effd
Ulysse Lemestré <ulysselem@yahoo.com>
2019-10-11 14:04:07 +0200

    Game 1 - def calculate above 4

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -21,11 +21,7 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
       }
       else if (m_score1>=4 || m_score2>=4)
       {
-        val minusResult = m_score1-m_score2
-        if (minusResult==1) score ="Advantage player1"
-        else if (minusResult == -1) score ="Advantage player2"
-        else if (minusResult>=2) score = "Win for player1"
-        else score ="Win for player2"
+        score = calculateScoreAboveFour()
       }
       else
       {
@@ -52,6 +48,14 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
       case 2 => "Thirty-All"
       case _ => "Deuce"
     }
+  }
+
+  def calculateScoreAboveFour() : String = {
+    val minusResult = m_score1-m_score2
+    if (minusResult==1) return "Advantage player1"
+    else if (minusResult == -1) return "Advantage player2"
+    else if (minusResult>=2) return "Win for player1"
+    else return "Win for player2"
   }
 
 }
