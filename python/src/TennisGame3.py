230c84f30584b26e4e2e64367b3de865bd7295a9
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 15:37:28 +0200

    Tennis3: simplify if-else logic

--- python/src/TennisGame3.py
+++ python/src/TennisGame3.py
@@ -15,21 +15,21 @@ class TennisGame3:
             self.pointsPlayer2 += 1
 
     def score(self):
-        if (self.pointsPlayer1 < 4 and self.pointsPlayer2 < 4) and (self.pointsPlayer1 + self.pointsPlayer2 < 6):
+        if (self.pointsPlayer1 == self.pointsPlayer2) and (self.pointsPlayer1 >= 3):
+            return "Deuce"
+        elif self.pointsPlayer1 < 4 and self.pointsPlayer2 < 4:
             points_list = ["Love", "Fifteen", "Thirty", "Forty"]
             player1_score = points_list[self.pointsPlayer1]
             if self.pointsPlayer1 == self.pointsPlayer2:
                 return player1_score + "-All"
             else:
                 return player1_score + "-" + points_list[self.pointsPlayer2]
-        elif self.pointsPlayer1 == self.pointsPlayer2:
-            return "Deuce"
         else:
             if self.pointsPlayer1 > self.pointsPlayer2:
                 winner_name = self.player1Name
             else:
                 winner_name = self.player2Name
-            if (self.pointsPlayer1 - self.pointsPlayer2) * (self.pointsPlayer1 - self.pointsPlayer2) == 1:
+            if abs(self.pointsPlayer1 - self.pointsPlayer2) == 1:
                 return "Advantage " + winner_name
             else:
                 return "Win for " + winner_name
