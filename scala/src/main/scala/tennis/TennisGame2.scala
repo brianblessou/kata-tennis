f98e4487e8d5f777b79b83534b5f17a417a5e942
Pierre Caeymaex <pierre.caeymaex@businessdecision.be>
2019-10-11 17:04:11 +0200

    game2 : cleaning code

--- scala/src/main/scala/tennis/TennisGame2.scala
+++ scala/src/main/scala/tennis/TennisGame2.scala
@@ -7,57 +7,24 @@ class TennisGame2 (val player1Name : String, val player2Name : String) extends T
     private var P1point = 0
     private var P2point  = 0
 
-    private var P1res = ""
-    private var P2res = ""
-
     def calculateScore() : String = {
-        var score = ""
-        if (P1point == P2point) {
-          if(P1point < 3) score = lowEqual(P1point)
-          else if(P1point>=3) score = "Deuce"
-          else throw new Exception("score not in range of valid values")
-        }else if (P1point > 0 && P2point==0)
-        {
-          score = love(P1point, player1Name)
-        }else if (P2point > 0 && P1point==0)
-        {
-          score = love(P2point, player2Name)
-        }else if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty"
-            if (P1point==3)
-                P1res="Forty"
-            if (P2point==1)
-                P2res="Fifteen"
-            if (P2point==2)
-                P2res="Thirty"
-            score = P1res + "-" + P2res
-        }else if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty"
-            if (P2point==3)
-                P2res="Forty"
-            if (P1point==1)
-                P1res="Fifteen"
-            if (P1point==2)
-                P1res="Thirty"
-            score = P1res + "-" + P2res
-        }else if (P1point > P2point && P2point >= 3)
-        {
-            score = "Advantage player1"
-        }else if (P2point > P1point && P1point >= 3)
-        {
-            score = "Advantage player2"
-        }else if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
-        {
-            score = "Win for player1"
-        }else if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
-        {
-            score = "Win for player2"
-        }
-        return score
+        if (P1point == P2point)
+          equal(P1point)
+        else if (P1point > 0 && P1point < 4 && P2point==0)
+          scoringLove(P1point, player1Name)
+        else if (P2point > 0 && P2point < 4 && P1point==0)
+          scoringLove(P2point, player2Name)
+        else if ((P1point < P2point && P2point < 4) || (P2point < P1point && P1point < 4))
+          scoring(P1point) + "-" + scoring(P2point)
+        else if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+          "Win for player1"
+        else if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+          "Win for player2"
+        else if (P1point > P2point && P2point >= 3)
+            "Advantage player1"
+        else if (P2point > P1point && P1point >= 3)
+            "Advantage player2"
+        else throw new Exception("score not in range of valid values")
     }
 
     def SetP1Score(number : Int) {
@@ -82,27 +49,44 @@ class TennisGame2 (val player1Name : String, val player2Name : String) extends T
         P1point += 1
     }
 
+  /**
+   *
+   */
     def P2Score(){
         P2point +=1
     }
 
+  /**
+   *
+   * @param player
+   */
     def wonPoint(player : String) {
-        if (player == "player1")
-            P1Score()
-        else
-            P2Score()
+        if (player == "player1") P1Score()
+        else P2Score()
     }
 
+  /**
+   *
+   * @param point
+   * @return
+   */
+  def equal(point: Int) : String = {
+    if(point < 3) lowEqual(point)
+    else if(point>=3) "Deuce"
+    else throw new Exception("score not in range of valid values")
+  }
+
+  /**
+   *
+   * @param point
+   * @return
+   */
     def lowEqual(point : Int) : String = {
-      var score = ""
-      if (P1point==0)
-        score = "Love"
-      if (P1point==1)
-        score = "Fifteen"
-      if (P1point==2)
-        score = "Thirty"
-      score += "-All"
-      score
+      if (P1point==0) "Love-All"
+      else if (P1point==1) "Fifteen-All"
+      else if (P1point==2) "Thirty-All"
+      else throw new Exception("score not in range of valid values")
+
     }
 
   /**
@@ -111,20 +95,21 @@ class TennisGame2 (val player1Name : String, val player2Name : String) extends T
    * @param playerName
    * @return
    */
-  def love(point : Int, playerName : String) : String = {
-    var res = ""
-    if (point==1)
-      res = "Fifteen"
-    if (point==2)
-      res = "Thirty"
-    if (point==3)
-      res = "Forty"
+  def scoringLove(point : Int, playerName : String) : String = {
+    if(playerName == player2Name) "Love" + "-" + scoring(point)
+    else scoring(point) + "-" + "Love"
+  }
 
-    if(playerName == player1Name){
-      res += "Love" + "-" + res
-    } else {
-      res += res + "-" + "Love"
-    }
-    res
+  /**
+   *
+   * @param point
+   * @return
+   */
+  def scoring(point : Int) : String = {
+    println(point)
+    if (point==1) "Fifteen"
+    else if (point==2) "Thirty"
+    else if (point==3) "Forty"
+    else throw new Exception("score not in range of valid values")
   }
 }
