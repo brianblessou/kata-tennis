5c1ad9a42e185d070c26a4ed90ec24455d98b31b
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 11:37:37 +0200

    split score in smaller functions

--- python/src/TennisGame1.py
+++ python/src/TennisGame1.py
@@ -14,36 +14,45 @@ class TennisGame1:
         else:
             self.p2points += 1
 
-    def score(self):
-        result = ""
-        tempScore = 0
-        if (self.p1points == self.p2points):
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
+    def equal_score(self):
+        return {
+            0: "Love-All",
+            1: "Fifteen-All",
+            2: "Thirty-All",
             }.get(self.p1points, "Deuce")
-        elif (self.p1points >= 4 or self.p2points >= 4):
-            minusResult = self.p1points - self.p2points
-            if (minusResult == 1):
-                result = "Advantage " + self.player1Name
-            elif (minusResult == -1):
-                result = "Advantage " + self.player2Name
-            elif (minusResult >= 2):
-                result = "Win for " + self.player1Name
-            else:
-                result = "Win for " + self.player2Name
+
+    def score_more_than_4(self):
+        minusResult = self.p1points - self.p2points
+        if minusResult == 1:
+            result = "Advantage " + self.player1Name
+        elif minusResult == -1:
+            result = "Advantage " + self.player2Name
+        elif minusResult >= 2:
+            result = "Win for " + self.player1Name
         else:
-            for i in range(1, 3):
-                if (i == 1):
-                    tempScore = self.p1points
-                else:
-                    result += "-"
-                    tempScore = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[tempScore]
+            result = "Win for " + self.player2Name
         return result
+
+    def diff_score(self):
+        result = ''
+        for i in range(1, 3):
+            if i == 1:
+                tempScore = self.p1points
+            else:
+                result += "-"
+                tempScore = self.p2points
+            result += {
+                0: "Love",
+                1: "Fifteen",
+                2: "Thirty",
+                3: "Forty",
+            }[tempScore]
+        return result
+
+    def score(self):
+        if (self.p1points == self.p2points):
+            return self.equal_score()
+        elif (self.p1points >= 4 or self.p2points >= 4):
+            return self.score_more_than_4()
+        else:
+            return self.diff_score()
