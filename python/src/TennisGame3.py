81ee8dd260f76b552a64e5841f146749b12cac5c
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 15:16:44 +0200

    Rename Variables Tennis3

--- python/src/TennisGame3.py
+++ python/src/TennisGame3.py
@@ -8,19 +8,19 @@ class TennisGame3:
         self.pointsPlayer1 = 0
         self.pointsPlayer2 = 0
 
-    def won_point(self, n):
-        if n == self.player1Name:
+    def won_point(self, winner_name):
+        if winner_name == self.player1Name:
             self.pointsPlayer1 += 1
         else:
             self.pointsPlayer2 += 1
 
     def score(self):
         if (self.pointsPlayer1 < 4 and self.pointsPlayer2 < 4) and (self.pointsPlayer1 + self.pointsPlayer2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.pointsPlayer1]
-            return s + "-All" if (self.pointsPlayer1 == self.pointsPlayer2) else s + "-" + p[self.pointsPlayer2]
+            points_list = ["Love", "Fifteen", "Thirty", "Forty"]
+            player1_score = points_list[self.pointsPlayer1]
+            return player1_score + "-All" if (self.pointsPlayer1 == self.pointsPlayer2) else player1_score + "-" + points_list[self.pointsPlayer2]
         else:
             if self.pointsPlayer1 == self.pointsPlayer2:
                 return "Deuce"
-            s = self.player1Name if self.pointsPlayer1 > self.pointsPlayer2 else self.player2Name
-            return "Advantage " + s if ((self.pointsPlayer1 - self.pointsPlayer2) * (self.pointsPlayer1 - self.pointsPlayer2) == 1) else "Win for " + s
+            player1_score = self.player1Name if self.pointsPlayer1 > self.pointsPlayer2 else self.player2Name
+            return "Advantage " + player1_score if ((self.pointsPlayer1 - self.pointsPlayer2) * (self.pointsPlayer1 - self.pointsPlayer2) == 1) else "Win for " + player1_score
