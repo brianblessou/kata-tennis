8526b6fe1f7348de1ff488c75a26b6ec3bb99965
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 14:38:40 +0200

    Adding Factory method

--- python/src/TennisGame1/main.py
+++ python/src/TennisGame1/main.py
@@ -18,12 +18,21 @@ class TennisGame1:
             self.p2points += 1
 
     def score(self):
-        if self.p1points == self.p2points:
-            scorer = EqualScore(self.p1points)
-        elif self.p1points >= 4 or self.p2points >= 4:
-            scorer = ScoreMoreThan4(self.player1Name, self.player2Name, self.p1points, self.p2points)
-        else:
-            scorer = DiffScore(self.p1points, self.p2points)
+        scorer = Factory(self.player1Name, self.player2Name, self.p1points, self.p2points).create_scorer()
         return scorer.compute_score()
 
 
+class Factory:
+    def __init__(self, player1Name, player2Name, p1points, p2points):
+        self.player1Name = player1Name
+        self.player2Name = player2Name
+        self.p1points = p1points
+        self.p2points = p2points
+
+    def create_scorer(self):
+        if self.p1points == self.p2points:
+            return EqualScore(self.p1points)
+        elif self.p1points >= 4 or self.p2points >= 4:
+            return ScoreMoreThan4(self.player1Name, self.player2Name, self.p1points, self.p2points)
+        else:
+            return DiffScore(self.p1points, self.p2points)
