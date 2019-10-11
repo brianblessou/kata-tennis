13994d5d86fa933106eb4e72f04c9c523fd5a99c
Thomas Dekelver <thomas.dekelver@businessdecision.be>
2019-10-11 15:52:40 +0200

    Tennis3: split in multiple functions

--- python/src/TennisGame3.py
+++ python/src/TennisGame3.py
@@ -14,22 +14,40 @@ class TennisGame3:
         else:
             self.pointsPlayer2 += 1
 
-    def score(self):
+    def points_lower_than_4(self) -> str:
+        """
+        Compute score when both players have scores lower than 4, except when 3-3
+        :return: Actual score (str)
+        """
+        points_list = ["Love", "Fifteen", "Thirty", "Forty"]
+        player1_score = points_list[self.pointsPlayer1]
+        if self.pointsPlayer1 == self.pointsPlayer2:
+            return player1_score + "-All"
+        else:
+            return player1_score + "-" + points_list[self.pointsPlayer2]
+
+    def points_bigger_than_4(self):
+        """
+        Compute score when score of at least one player is bigger than 4 and scores are not equal
+        :return: Actual score (str)
+        """
+        if self.pointsPlayer1 > self.pointsPlayer2:
+            winner_name = self.player1Name
+        else:
+            winner_name = self.player2Name
+        if abs(self.pointsPlayer1 - self.pointsPlayer2) == 1:
+            return "Advantage " + winner_name
+        else:
+            return "Win for " + winner_name
+
+    def score(self) -> str:
+        """
+        Computes for different case the score of the game
+        :return: Actual score (str)
+        """
         if (self.pointsPlayer1 == self.pointsPlayer2) and (self.pointsPlayer1 >= 3):
             return "Deuce"
         elif self.pointsPlayer1 < 4 and self.pointsPlayer2 < 4:
-            points_list = ["Love", "Fifteen", "Thirty", "Forty"]
-            player1_score = points_list[self.pointsPlayer1]
-            if self.pointsPlayer1 == self.pointsPlayer2:
-                return player1_score + "-All"
-            else:
-                return player1_score + "-" + points_list[self.pointsPlayer2]
+            return self.points_lower_than_4()
         else:
-            if self.pointsPlayer1 > self.pointsPlayer2:
-                winner_name = self.player1Name
-            else:
-                winner_name = self.player2Name
-            if abs(self.pointsPlayer1 - self.pointsPlayer2) == 1:
-                return "Advantage " + winner_name
-            else:
-                return "Win for " + winner_name
+            return self.points_bigger_than_4()
