88346ec0e6a113111a30b79ee5bb94a99e16b30d
robin <robin.cavalieri@businessdecision.be>
2019-10-11 11:45:29 +0200

    finish code review for Tennis1

--- scala/src/main/scala/tennis/TennisGame1.scala
+++ scala/src/main/scala/tennis/TennisGame1.scala
@@ -19,13 +19,32 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
    * @author Robin CAVALIERI
    */
   def equalScore(score: Int) : String = {
-    val scoreConv = score match {
+    score match {
         case 0 =>  "Love-All"
         case 1 => "Fifteen-All"
         case 2 => "Thirty-All"
         case _ => "Deuce"
     }
-    scoreConv
+  }
+
+  /**
+   *
+   * @param minusResult
+   * @return
+   */
+  def getMatchStatus(minusResult: Int) : String = {
+    if (minusResult == 1) {
+      "Advantage player1"
+    }
+    else if (minusResult == -1) {
+      "Advantage player2"
+    }
+    else if (minusResult >= 2) {
+      "Win for player1"
+    }
+    else {
+      "Win for player2"
+    }
   }
 
   /**
@@ -36,22 +55,7 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
    * @author Robin CAVALIERI
    */
   def winningScore(scorePlayer1: Int, scorePlayer2: Int) : String = {
-    var score = ""
-    val minusResult = scorePlayer1-scorePlayer2
-
-    if (minusResult == 1) {
-      score = "Advantage player1"
-    }
-    else if (minusResult == -1) {
-      score = "Advantage player2"
-    }
-    else if (minusResult >= 2) {
-      score = "Win for player1"
-    }
-    else {
-      score = "Win for player2"
-    }
-    score
+    getMatchStatus(scorePlayer1-scorePlayer2)
   }
 
   /**
@@ -60,15 +64,12 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
    * @return
    */
   def convScore(score: Int): String = {
-
-    val scoreConv = score match {
+    score match {
       case 0 => "Love"
       case 1 => "Fifteen"
       case 2 => "Thirty"
       case 3 => "Forty"
     }
-
-    scoreConv
   }
 
   /**
@@ -84,25 +85,22 @@ class TennisGame1 (val player1Name : String, val player2Name : String) extends T
 
   /**
    *
-   * @desc depending on the case call diffScore / winningScore / equalScore
    * @return
    */
   def calculateScore() : String = {
-      var score : String = ""
 
       if (m_score1 == m_score2)
       {
-          score = equalScore(m_score1)
+          equalScore(m_score1)
       }
       else if (m_score1>=4 || m_score2>=4)
       {
-          score = winningScore(m_score1, m_score2)
+          winningScore(m_score1, m_score2)
       }
       else
       {
-          score = diffScore(m_score1, m_score2)
+          diffScore(m_score1, m_score2)
       }
-      score
   }
 
 }
